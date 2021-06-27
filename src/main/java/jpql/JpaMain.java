181b56a39046d5f51817938097e3a3797c756e34
jinho <g1063114@naver.com>
2021-06-27 11:33:08 +0900

    type binding

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,7 +17,9 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
 
             tx.commit();
         }catch (Exception e){
